3fa2f0f2ed368bdbc06275ce972804adbc059f88
murilo <murilocarrijoadm@gmail.com>
2022-05-24 16:23:31 -0300

    implementando metodos para verificação de data

--- back-end/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/exercicio-4/Date.ts
+++ back-end/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/exercicio-4/Date.ts
@@ -1,4 +1,4 @@
-export default class Data {
+export default class Dates {
   private _day: number;
   private _month: number;
   private _year: number;
@@ -28,6 +28,8 @@ export default class Data {
     return this._month;
   }
   public set month(value: number) {
+    if (value > 12) throw new Error('O mês informado é inválido');
+    
     this._month = value;
   }
 
@@ -38,7 +40,53 @@ export default class Data {
     this._year = value;
   }
 
+  getMonthName(): string {
+    const months = [
+      'janeiro',
+      'fevereiro',
+      'março',
+      'abril',
+      'maio',
+      'junho',
+      'julho',
+      'agosto',
+      'setembro',
+      'outubro',
+      'novembro',
+      'dezembro',
+    ];
+
+    return months[this.month - 1];
+  }
+
+  isLeapYear(): boolean {
+    return this._year % 4 === 0;
+  }
+
+  verifyDate(date: Dates): number {
+    const currentDate = `${this._year}-${this._month}-${this._day}`;
+    const newDate = `${date.year}-${date.month}-${date.day}`;
+
+    if (new Date(currentDate) > new Date(newDate)) return 1;
+    if (new Date(currentDate) < new Date(newDate)) return -1;
+
+    return 0;
+  }
+
 }
 
-const data1 = new Data(17, 11, 1989);
+const data1 = new Dates(17, 10, 1988);
 console.log(data1);
+console.log('Dia: ', data1.day);
+console.log('Mês: ', data1.month);
+console.log('Ano: ', data1.year);
+console.log('Mês por extenso: ', data1.getMonthName());
+console.log('Ano bissexto: ', data1.isLeapYear());
+
+const otherDate = new Dates(30, 1, 2021);
+
+const verify = data1.verifyDate(otherDate);
+const compareStates = ['anterior', 'igual', 'posterior'];
+console.log(`A primeira data é ${compareStates[verify + 1]} a segunda.`);
+
+
