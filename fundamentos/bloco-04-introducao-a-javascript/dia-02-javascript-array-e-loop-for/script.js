f1a694786522f393e7264e88780a6b952de0a3f9
murilo <murilocarrijoadm@gmail.com>
2022-10-10 17:53:25 -0300

    feat: resolução dos exercícios 5 do dia 02

--- fundamentos/bloco-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
@@ -6,7 +6,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 '1 - Percorra o array imprimindo todos os valores nele contidos com a função console.log();'
 
 for (number of numbers) {
-  console.log('Exercício 1', number);
+  console.log('Exercício 1:', number);
 }
 
 '2 - Some todos os valores contidos no array e imprima o resultado'
@@ -16,7 +16,7 @@ for (number of numbers) {
   sum = sum + number;
 }
 
-console.log('Exercício 2', sum);
+console.log('Exercício 2:', sum);
 
 '3 - Calcule e imprima a média aritmética dos valores contidos no array;'
   'A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.'
@@ -27,12 +27,23 @@ for (number of numbers) {
   sum2 = sum2 + number;
 }
 
-console.log('Exercício 3', sum2/numbers.length);
+console.log('Exercício 3:', sum2/numbers.length);
 
 '4 - Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”'
 
 if (sum2 > 20) {
-  console.log('Exercício 4: ', 'valor maior que 20');
+  console.log('Exercício 4:', 'valor maior que 20');
 } else {
-  console.log('Exercício 4: ', 'valor menor ou igual a 20');
+  console.log('Exercício 4:', 'valor menor ou igual a 20');
 }
+
+'5 - Utilizando for, descubra qual o maior valor contido no array e imprima-o'
+
+let higherNumber = 0;
+for (let i = 0; i <= numbers.length; i += 1) {
+  if (numbers[i] > higherNumber) {
+    higherNumber = numbers[i];
+  }
+}
+
+console.log('Exercício 5:', higherNumber);
