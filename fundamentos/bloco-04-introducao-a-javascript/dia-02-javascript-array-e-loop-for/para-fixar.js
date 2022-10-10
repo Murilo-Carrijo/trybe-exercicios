675f85e3d4f22ad53717b15542c27d825e2a4c8d
murilo <murilocarrijoadm@gmail.com>
2022-10-10 17:28:11 -0300

    feat: resolução dos exercícios de fixação referentes ao for/of

--- fundamentos/bloco-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/para-fixar.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/para-fixar.js
@@ -28,3 +28,14 @@ let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 for (let index = 0; index < groceryList.length; index += 1) {
     console.log('Exercício 1 for: ', groceryList[index]);   
 }
+
+'For/of'
+
+'1 - Utilize o for/of para imprimir os elementos da lista names com o console.log():'
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (const name of names) {
+    console.log('Exercicio 1 for/of: ', name);
+}
+
