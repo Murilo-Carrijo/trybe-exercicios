fc5952fd5e237a8cca473165dc8b606b312d6e19
murilo <murilocarrijoadm@gmail.com>
2022-10-10 11:28:40 -0300

    feat: feat: exercicios de fixação sobre Switch e Case

--- fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/para-fixar.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/para-fixar.js
@@ -100,3 +100,29 @@ if (weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'qu
 } else if (weekDay === 'sabado' || weekDay === 'domingo') {
   console.log('FINALMENTE, descanso merecido UwU');
 }
+
+'Switch e Case'
+
+'1 - Crie uma variável para armazenar o status da pessoa candidata no processo seletivo, que pode ser: "aprovada", "lista" ou "reprovada";'
+const status = 'aprovado';
+
+'2 - Crie uma estrutura condicional com o switch/case que irá imprimir as seguintes mensagens:'
+    'Caso "aprovada", imprima “Parabéns, você foi aprovada(o)!.'
+    'Caso "lista", imprima “Você está na nossa lista de espera.'
+    'Caso "reprovada", imprima “Você foi reprovada(o).'
+    'Caso default, imprima a mensagem de “Informação incorreta”.'
+
+switch (status) {
+  case 'aprovado':
+    console.log('Parabéns, você foi aprovada(o)!.');
+    break;
+  case 'lista':
+    console.log('Você está na nossa lista de espera.');
+    break;
+  case 'reprovado':
+    console.log('Você foi reprovada(o).');
+    break;
+  default:
+    console.log('Informação incorreta.');
+    break;
+}
