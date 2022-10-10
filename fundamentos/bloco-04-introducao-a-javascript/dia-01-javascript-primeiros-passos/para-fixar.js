fcccaa41081eae828d49a3ed75f0f40210941a76
murilo <murilocarrijoadm@gmail.com>
2022-10-10 10:45:57 -0300

    feat: feat: exercicios de fixação sobre Tipos Primitivos, Tipagem Dinâmica e Operações Aritméticas

--- fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/para-fixar.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/para-fixar.js
@@ -14,6 +14,23 @@ console.log(birthYear);
 birthYear = '2030';
 console.log('atualizado:', birthYear);
 '6 - Altere o valor atribuído à constante birthCity. Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro? 🤔'
-birthCity = 'São Paulo';
-console.log('atualizado:', birthCity);
+// birthCity = 'São Paulo';
+// console.log('atualizado:', birthCity);
     'Resposta: o erro ocorre pois a vaiável foi criada como constaten, ou seja, não podemos atribuir um novo valor a ela.'
+
+
+'Tipos Primitivos, Tipagem Dinâmica e Operações Aritméticas:'
+
+'1 - Crie uma constante chamada base e atribua a ela o valor 5.'
+const base = 5;
+
+'2 - Crie uma constante uma chamada heigth e atribua a ela o valor 8.'
+const height = 8;
+
+'3 - Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela heigth. Dica: lembre-se de usar ~~sempre~~ o console.log() para imprimir as variáveis e checar os resultados das operações!'
+const area  = base * height;
+console.log(area);
+
+'4 - Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.'
+const perimeter = 5 + 5 + 8 + 8;
+console.log(perimeter);
