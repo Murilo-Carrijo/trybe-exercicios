4cc87d2c7e7149ea7eade0f116a1eafc5b19f42f
murilo <murilocarrijoadm@gmail.com>
2022-10-10 17:41:28 -0300

    feat: resolução dos exercícios 2 do dia 02

--- fundamentos/bloco-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
@@ -8,3 +8,12 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (number of numbers) {
   console.log('Exercício 1', number);
 }
+
+'2 - Some todos os valores contidos no array e imprima o resultado'
+
+let sum = 0;
+for (number of numbers) {
+  sum = sum + number;
+}
+
+console.log('Exercício 2', sum);
