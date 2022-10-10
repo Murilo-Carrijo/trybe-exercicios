75c204f58e12f7b679a582105caa39c7fd6c4a74
murilo <murilocarrijoadm@gmail.com>
2022-10-10 19:10:49 -0300

    feat: resolução do primeiro exercicio bonus

--- fundamentos/bloco-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script-bonus.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script-bonus.js
@@ -1,1 +1,17 @@
 'Exercícios - Bônus - Ordenação Bubble Sort'
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+'1 - Ordene o array numbers em ordem crescente e imprima seus valores;'
+
+for (let firstI = 1; firstI < numbers.length; firstI += 1) {
+  for (let secondI = 0; secondI < numbers.length; secondI += 1) {
+    if (numbers[firstI] < numbers[secondI]) {
+      let current = numbers[firstI];
+      numbers[firstI] = numbers[secondI];
+      numbers[secondI] = current;
+    }
+  }
+}
+
+console.log('Exercício 1:', numbers);
