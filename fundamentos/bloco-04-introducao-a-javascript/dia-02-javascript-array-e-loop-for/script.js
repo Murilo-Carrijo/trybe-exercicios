595212b8a6457936ed649636bff3882369b79b55
murilo <murilocarrijoadm@gmail.com>
2022-10-10 17:39:30 -0300

    feat: resolução dos exercícios 1 do dia 02

---
+++ fundamentos/bloco-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
@@ -0,0 +1,10 @@
+'🚀 Lidando com Arrays'
+'s utilizar esse array para realizar os próximos exercícios.'
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+'1 - Percorra o array imprimindo todos os valores nele contidos com a função console.log();'
+
+for (number of numbers) {
+  console.log('Exercício 1', number);
+}
