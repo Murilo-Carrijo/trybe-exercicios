820d688eecd74f250d25f956fb11662c2026ea7b
murilo <murilocarrijoadm@gmail.com>
2022-10-10 19:11:40 -0300

    feat: resolução do segundo exercicio bonus

--- fundamentos/bloco-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script-bonus.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script-bonus.js
@@ -15,3 +15,17 @@ for (let firstI = 1; firstI < numbers.length; firstI += 1) {
 }
 
 console.log('Exercício 1:', numbers);
+
+'2 - rdene o array numbers em ordem decrescente e imprima seus valores;'
+
+for (let firstI = 1; firstI < numbers.length; firstI += 1) {
+  for (let secondI = 0; secondI < numbers.length; secondI += 1) {
+    if (numbers[firstI] > numbers[secondI]) {
+      let current = numbers[firstI];
+      numbers[firstI] = numbers[secondI];
+      numbers[secondI] = current;
+    }
+  }
+}
+
+console.log('Exercício 2:', numbers);
