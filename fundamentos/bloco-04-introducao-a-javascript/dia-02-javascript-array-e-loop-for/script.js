f67040424d8c30d3e0a3e0ef9e6210ff2f3d036a
murilo <murilocarrijoadm@gmail.com>
2022-10-10 17:48:40 -0300

    feat: resolução dos exercícios 4 do dia 02

--- fundamentos/bloco-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
@@ -28,3 +28,11 @@ for (number of numbers) {
 }
 
 console.log('Exercício 3', sum2/numbers.length);
+
+'4 - Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”'
+
+if (sum2 > 20) {
+  console.log('Exercício 4: ', 'valor maior que 20');
+} else {
+  console.log('Exercício 4: ', 'valor menor ou igual a 20');
+}
