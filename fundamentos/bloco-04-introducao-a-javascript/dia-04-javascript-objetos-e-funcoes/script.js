2e01319a89ccbc8fd718e8c3041f0999a7773839
murilo <murilocarrijoadm@gmail.com>
2022-10-11 16:35:50 -0300

    feat: resolução do exercício 3 do dia 04

--- fundamentos/bloco-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/script.js
@@ -8,10 +8,16 @@ let info = {
 
 '1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Use a sintaxe meuObjeto.chave. Valor esperado no console:'
 
-console.log('Exercício 1: ', 'Bem-vinda, ' + info.personagem);
+console.log('Exercício 1:', 'Bem-vinda, ' + info.personagem);
 
 '2 - Insira no objeto uma nova propriedade com o nome de chave "recorrente" e o valor "Sim" e, em seguida, imprima o objeto no console. Use a sintaxe meuObjeto["chave"] = valor. Valor esperado no console:'
 
 info['recorrente'] = 'Sim';
 
-console.log('Exercício 2: ', info);
+console.log('Exercício 2:', info);
+
+'3 - Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:'
+
+for (const key in info) {
+  console.log('Exercício 3:', key);
+}
