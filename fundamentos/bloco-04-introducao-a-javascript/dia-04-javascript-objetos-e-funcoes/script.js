5703da6943dc0de4876fc76e6cd7ae37428b444c
murilo <murilocarrijoadm@gmail.com>
2022-10-11 16:34:37 -0300

    feat: resolução do exercício 2 do dia 04

--- fundamentos/bloco-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/script.js
@@ -8,4 +8,10 @@ let info = {
 
 '1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Use a sintaxe meuObjeto.chave. Valor esperado no console:'
 
-console.log('Seja bem vindo(a) ' + info.personagem);
+console.log('Exercício 1: ', 'Bem-vinda, ' + info.personagem);
+
+'2 - Insira no objeto uma nova propriedade com o nome de chave "recorrente" e o valor "Sim" e, em seguida, imprima o objeto no console. Use a sintaxe meuObjeto["chave"] = valor. Valor esperado no console:'
+
+info['recorrente'] = 'Sim';
+
+console.log('Exercício 2: ', info);
