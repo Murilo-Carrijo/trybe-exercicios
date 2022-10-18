ba2d7788da5c7e296261a554de95d4833382d0b1
murilo <murilocarrijoadm@gmail.com>
2022-10-18 16:29:01 -0300

    feat: exercício de fixação referente a seletores e css concluído

---
+++ fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-01-javascript-dom-e-seletores/exercises/seletores.js
@@ -0,0 +1,23 @@
+const body = document.getElementById('container');
+body.style.backgroundColor = '#fffffe';
+
+const header = document.getElementById('header-container');
+header.style.backgroundColor = '#2cb67d';
+
+const emergencyTask = document.querySelector('.emergency-tasks');
+emergencyTask.style.backgroundColor = '#fec7d7';
+
+const noEmergencyTask = document.querySelector('.no-emergency-tasks');
+noEmergencyTask.style.backgroundColor = '#ffd803';
+
+const h3 = document.getElementsByTagName('h3');
+h3[0].style.backgroundColor = '#a786df';
+h3[1].style.backgroundColor = '#a786df';
+h3[2].style.backgroundColor = '#0e172c';
+h3[3].style.backgroundColor = '#0e172c';
+
+const footer = document.getElementById('footer-container');
+footer.style.backgroundColor = '#00473e';
+
+
+console.log(h3);
