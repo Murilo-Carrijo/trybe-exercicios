64534ebcd65d19107f67eee55d4f20a2d54ccd7b
murilo <murilocarrijoadm@gmail.com>
2022-10-19 15:18:59 -0300

    feat: resolucao do exercicio 10 do dia 2

--- fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-02-javascript-trabalho-com-elementos/criando-elementos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-02-javascript-trabalho-com-elementos/criando-elementos/script.js
@@ -4,7 +4,13 @@ const quintoFilho = document.createElement('section');
 quintoFilho.id = 'quintoFilho';
 quintoFilho.innerHTML = 'Quinto Filho';
 pai.appendChild(quintoFilho)
-console.log(quintoFilho);
 //10 - Crie um filho para elementoOndeVoceEsta.
+const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+const terceiroFilhoDoFilho = document.createElement('section');
+terceiroFilhoDoFilho.id = 'terceiroFilhoDoFilho';
+terceiroFilhoDoFilho.innerHTML = 'terceiroFilhoDoFilho';
+elementoOndeVoceEsta.appendChild(terceiroFilhoDoFilho);
+console.log(elementoOndeVoceEsta);
+
 //11 - Crie um filho para primeiroFilhoDoFilho.
 //12 - A partir desse filho criado, acesse terceiroFilho.
