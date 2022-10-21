7cd9a70627dac49e01bec1fbaf34d54a9911f7e4
murilo <murilocarrijoadm@gmail.com>
2022-10-21 15:28:23 -0300

    resolução do exercício 5 de fixação referente ao dia 3

--- fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-03-javascript-eventos/para-fixar/addEventListener/main.js
+++ fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-03-javascript-eventos/para-fixar/addEventListener/main.js
@@ -42,8 +42,19 @@ function redirectLinkedin() {
 
 myWebpage.addEventListener('dblclick', redirectLinkedin);
 
-// - Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+// 5 - Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+
+function changeColor(event) {
+  event.target.style.color = '#2fc18c';
+}
+
+function removeColor(event) {
+  event.target.style.color = '#fffffe';
+}
+
+myWebpage.addEventListener('mouseover', changeColor);
+myWebpage.addEventListener('mouseleave', removeColor);
 
 // Segue abaixo um exemplo do uso de event.target:
 
