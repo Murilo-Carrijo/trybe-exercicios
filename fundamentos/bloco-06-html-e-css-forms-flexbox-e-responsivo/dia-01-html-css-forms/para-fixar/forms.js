2f1a6e8d87e165bb98805eb3ee8907410c418f43
murilo <murilocarrijoadm@gmail.com>
2022-11-01 16:54:06 -0300

    feat: criando titulo para a pagina de login dia 01 bloco 06

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/para-fixar/forms.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/para-fixar/forms.js
@@ -1,1 +1,36 @@
-console.log('olá');
+// Para fixar
+// Vamos praticar um pouco, criando uma tela de login? 🔥
+function title() {
+  const body = document.getElementsByTagName('body')[0];
+  const header = document.createElement('header');
+  header.id = 'header-container';
+  header.className = 'header-container';
+  header.style.backgroundColor = 'green';
+  header.style.textAlign = 'center';
+  body.appendChild(header);
+  const title = document.createElement('h1');
+  title.id = 'title';
+  title.className = 'title-text';
+  title.innerText = 'Login';
+  title.style.color = 'white';
+  header.appendChild(title);
+}
+
+// 1 - Adicione os campos de entrada para email e senha.
+
+
+// 2 - Adicione um checkbox com o texto Lembre-me.
+
+// 3 - Adicione um botão com o texto Entrar; crie o botão através da tag <input>.
+
+// 4 - Adicione os placeholder Digite seu email e Digite sua senha para os campos de email e senha.
+
+// 5 - Adicione o atributo required para os campos de email e senha.
+
+// Adicione o atributo autocomplete="off" para o campo de email.
+
+// Adicione o atributo autofocus para o campo de email.
+
+window.onload = () => {
+  title();
+}
