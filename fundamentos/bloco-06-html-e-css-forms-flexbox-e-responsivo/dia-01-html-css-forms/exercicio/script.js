9006ac17657e67d6337ef66625e7eb8edeb79243
murilo <murilocarrijoadm@gmail.com>
2022-11-02 11:05:25 -0300

    feat: criando função parar limpar todos os input e textarea  dia 1 bloco 6

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/exercicio/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/exercicio/script.js
@@ -5,6 +5,22 @@ function preventDefault() {
   });
 }
 
+function clearAll() {
+  const allInputs = document.querySelectorAll('input');
+  const textarea = document.querySelector('textarea');
+  const clearButton = document.getElementById('button-clear');
+
+  clearButton.addEventListener('click', (event) => {
+    for (let i = 0; i < allInputs.length; i += 1) {
+      const input = allInputs[i];
+      input.value = '';
+      input.checked = false;
+    }
+    textarea.value = '';
+  });
+}
+
 window.onload = () => {
   preventDefault();
+  clearAll();
 };
