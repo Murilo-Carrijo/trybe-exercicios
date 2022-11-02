3a5c0da7d19672ec6d17ebd2152186833aa6704a
murilo <murilocarrijoadm@gmail.com>
2022-11-02 09:42:34 -0300

    chore: iniciando segunda etapa dos exercícios de fixação

---
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/para-fixar/script.js
@@ -0,0 +1,13 @@
+// Para fixar:
+// Crie um arquivo HTML index.html.
+// Crie um arquivo Javascript script.js.
+// Em seu arquivo HTML:
+// Adicione o <script src="script.js"></script> no final do body do HTML.
+// Copie os três elementos abaixo (a, input type="text", checkbox) em sua página
+// Em seu arquivo Javascript.
+// Adicione os seletores abaixo no começo do arquivo.
+
+
+const INPUT_TEXT = document.querySelector("#input-text");
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+const HREF_LINK = document.querySelector("#href");
