2c7627446dec0d138ba7ce9cd89cd4186cccbcba
murilo <murilocarrijoadm@gmail.com>
2022-11-02 09:45:41 -0300

    feat: requisito 1 segunda etapa - adicionando o preventDefault a tag a

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/para-fixar/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/para-fixar/script.js
@@ -11,3 +11,11 @@
 const INPUT_TEXT = document.querySelector("#input-text");
 const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
 const HREF_LINK = document.querySelector("#href");
+
+
+// 1 - Adicione um evento click no elemento a que possui o id href. Na função do evento, implemente o método .preventDefault() para prevenir o comportamento padrão do elemento. Ou seja, ao clicar no link, nada deve acontecer.
+function testPreventDefault(event) {
+  event.preventDefault();
+}
+
+HREF_LINK.addEventListener('click', testPreventDefault);
