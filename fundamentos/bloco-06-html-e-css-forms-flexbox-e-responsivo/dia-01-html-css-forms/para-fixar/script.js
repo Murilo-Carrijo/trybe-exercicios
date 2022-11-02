312f25d0dafa5a143a636c0e5b51773f9f5fd4e4
murilo <murilocarrijoadm@gmail.com>
2022-11-02 09:51:18 -0300

    feat: requisito 3 segunda etapa - adicionando o preventDefault a tag input text permitindo somente o caracter a

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/para-fixar/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/para-fixar/script.js
@@ -23,3 +23,14 @@ HREF_LINK.addEventListener('click', testPreventDefault);
 // 2 - Adicione um evento click no elemento checkbox que possui o id input-checkbox. Na função do evento, implemente o método .preventDefault() para prevenir o comportamento padrão do elemento. Ou seja, ao clicar na caixinha, nada deve acontecer.
 
 INPUT_CHECKBOX.addEventListener('click', testPreventDefault);
+
+// 3 - Adicione um evento keypress no elemento input type="text" que possui o id input-text. Na função do evento, implemente o método .preventDefault() de forma que somente o caractere a (letra ‘a’ minúscula) possa ser digitado na caixinha. (dica: para capturar a tecla digitada, utilize o event.key)
+
+function getA(event) {
+  const character = event.key;
+  if (character !== 'a') {
+    event.preventDefault();
+  }
+}
+
+INPUT_TEXT.addEventListener('keypress', getA);
