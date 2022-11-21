864fc5e51b6f745bd0ebf36194865f85ddf506d3
murilo <murilocarrijoadm@gmail.com>
2022-11-21 20:11:06 -0300

    chore: bloco 7 dia 1 - iniciando os exercicios do dia

---
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-01-fluxo-de-excecoes-e-manipulacao-de-objetos/para-fixar/adicionando-novas-chaves/script.js
@@ -0,0 +1,1 @@
+// Agora que você viu diferentes formas de associar chaves e valores a um objeto, que tal praticar? Adicione suas informações ao objeto pessoaEstudante = {}, como nome, email, telefone, github e linkedIn, usando os métodos aprendidos neste conteúdo.
