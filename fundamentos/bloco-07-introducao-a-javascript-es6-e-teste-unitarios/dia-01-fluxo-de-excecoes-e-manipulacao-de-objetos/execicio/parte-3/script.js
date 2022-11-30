15ce19ac49cd7d7e4ea91cb8f0ecfd61b730dc65
murilo <murilocarrijoadm@gmail.com>
2022-11-30 18:36:00 -0300

    chore: bloco 7 dia 1 - exercicio parte 3 - requisito 1 editar o objeto lesson2

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-01-fluxo-de-excecoes-e-manipulacao-de-objetos/execicio/parte-3/script.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-01-fluxo-de-excecoes-e-manipulacao-de-objetos/execicio/parte-3/script.js
@@ -23,6 +23,13 @@ const lesson3 = {
 
 // 1 - Crie uma função para adicionar o turno da noite na lesson2. Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
 
+const editLesson = (lesson, key, value) => {
+  lesson[key] = value;
+  console.log(lesson);
+};
+
+editLesson(lesson2, 'turno', 'noite');
+
 // 2 - Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
 
 // 3 - Crie uma função para mostrar o tamanho de um objeto.
@@ -30,7 +37,7 @@ const lesson3 = {
 // 4 - Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 
 // 5 - Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign. Cada chave desse novo objeto será uma aula, sendo essas chaves lesson1, lesson2 e lesson3. Ao executar o comando console.log(allLessons), a saída deverá ser a seguinte:
-console.log(allLessons);
+// console.log(allLessons);
 /*
 {
   lesson1:
@@ -54,7 +61,7 @@ console.log(allLessons);
 // 6 - Usando o objeto criado no tópico anterior, crie uma função que retorne o número total de estudantes em todas as aulas.
 
 // 7 - Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
-console.log(getValueByNumber(lesson1, 0));
+// console.log(getValueByNumber(lesson1, 0));
 // Output: 'Matemática'
 
 // 8 - Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles o objeto, o nome da chave e o valor da chave. Exemplo:
