5bcac7f70697f507b4e0c81ff0313b17a5140cdf
murilo <murilocarrijoadm@gmail.com>
2022-11-30 18:46:03 -0300

    chore: bloco 7 dia 1 - exercicio parte 3 - requisito 3 retornar o tamanho do objeto

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-01-fluxo-de-excecoes-e-manipulacao-de-objetos/execicio/parte-3/script.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-01-fluxo-de-excecoes-e-manipulacao-de-objetos/execicio/parte-3/script.js
@@ -41,6 +41,13 @@ listKeys(lesson1);
 
 // 3 - Crie uma função para mostrar o tamanho de um objeto.
 
+const lengthObject = (lesson) => {
+  const numberKeys = Object.keys(lesson).length;
+  console.log('Tamanho do objeto:', numberKeys, 'itens');
+};
+
+lengthObject(lesson3);
+
 // 4 - Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 
 // 5 - Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign. Cada chave desse novo objeto será uma aula, sendo essas chaves lesson1, lesson2 e lesson3. Ao executar o comando console.log(allLessons), a saída deverá ser a seguinte:
