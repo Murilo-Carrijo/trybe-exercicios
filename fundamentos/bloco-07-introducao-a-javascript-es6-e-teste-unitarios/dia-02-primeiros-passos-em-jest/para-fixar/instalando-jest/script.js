757f65659b194eb0a47d9a483a6a045f48446ec2
murilo <murilocarrijoadm@gmail.com>
2022-12-01 17:20:04 -0300

    test: bloco 7 dia 2 - para fixar - Adicionando testes para subtração, muitiplicação e divisão.

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-02-primeiros-passos-em-jest/para-fixar/instalando-jest/script.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-02-primeiros-passos-em-jest/para-fixar/instalando-jest/script.js
@@ -2,4 +2,21 @@ const sum = (a , b) => {
   return a + b;
 };
 
-module.exports = sum;
+const subtract = (a, b) => {
+  return a - b;
+};
+
+const multiplication = (a, b) => {
+  return a * b;
+};
+
+const division = (a, b) => {
+  return a/b;
+};
+
+module.exports = {
+  sum,
+  subtract,
+  multiplication,
+  division,
+};
--- fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-02-primeiros-passos-em-jest/para-fixar/instalando-jest/sum.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-02-primeiros-passos-em-jest/para-fixar/instalando-jest/sum.test.js
@@ -1,5 +1,17 @@
-const sum = require('./script');
+const operations = require('./script');
 
 test('sums two values', () => {
-  expect(sum(2, 3)).toEqual(5);
+  expect(operations.sum(2, 3)).toEqual(5);
+});
+
+test('subtract two values', () => {
+  expect(operations.subtract(5, 4)).toEqual(1);
+});
+
+test('multiplication tow values', () => {
+  expect(operations.multiplication(2, 3)).toEqual(6);
+});
+
+test('division two values', () => {
+  expect(operations.division(4, 2)).toEqual(2);
 });
