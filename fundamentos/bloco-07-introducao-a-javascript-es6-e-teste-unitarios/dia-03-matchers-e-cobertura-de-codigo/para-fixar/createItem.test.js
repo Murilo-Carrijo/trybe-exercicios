d3e950ea9ced7cdd9b3e4ef69ca3ac9844d98f22
murilo <murilocarrijoadm@gmail.com>
2022-12-04 16:29:32 -0300

    test: bloco 7 dia 3 - para fixar - testando caso em que a quantidde não é passada para a função createItem

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-03-matchers-e-cobertura-de-codigo/para-fixar/createItem.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-03-matchers-e-cobertura-de-codigo/para-fixar/createItem.test.js
@@ -2,13 +2,16 @@ const createItem = require('./createItem');
 
 describe('a função createItem', () => {
   it('cria um item válido', () => {
-    const validInfo = createItem('Murilo', 2, 10.50, 2);
-    expect(validInfo).toHaveProperty('name', 'Murilo');
-    expect(validInfo).toHaveProperty('quantity', 2);
-    expect(validInfo).toHaveProperty('unit', 2);
-    expect(validInfo).toHaveProperty('price', 10.5);
+    const validInfo = createItem('banana', 'kg', 1.99, 20);
+    expect(validInfo).toHaveProperty('name', 'banana');
+    expect(validInfo).toHaveProperty('quantity', 20);
+    expect(validInfo).toHaveProperty('unit', 'kg');
+    expect(validInfo).toHaveProperty('price', 1.99);
   });
-  it.todo('utiliza zero como quantidade padrão');
+  it('utiliza zero como quantidade padrão',  () => {
+    const element = createItem('banana', 'kg', 1.99);
+    expect(element).toHaveProperty('quantity', 0);
+  });
   it.todo('Lança um erro quando não recebe parâmetros');
   it.todo('Lança um erro se o nome do item não é uma string');
   it.todo('Lança um erro se o preço é negativo');
