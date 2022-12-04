d8f9f0b5ba60ceba4677f588d5c9758ff4aa9d3e
murilo <murilocarrijoadm@gmail.com>
2022-12-04 16:19:51 -0300

    test: bloco 7 dia 3 - para fixar - testando caso de sucesso da função createItem

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-03-matchers-e-cobertura-de-codigo/para-fixar/createItem.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-03-matchers-e-cobertura-de-codigo/para-fixar/createItem.test.js
@@ -1,6 +1,13 @@
 const createItem = require('./createItem');
+
 describe('a função createItem', () => {
-  it.todo('cria um item válido');
+  it('cria um item válido', () => {
+    const validInfo = createItem('Murilo', 2, 10.50, 2);
+    expect(validInfo).toHaveProperty('name', 'Murilo');
+    expect(validInfo).toHaveProperty('quantity', 2);
+    expect(validInfo).toHaveProperty('unit', 2);
+    expect(validInfo).toHaveProperty('price', 10.5);
+  });
   it.todo('utiliza zero como quantidade padrão');
   it.todo('Lança um erro quando não recebe parâmetros');
   it.todo('Lança um erro se o nome do item não é uma string');
