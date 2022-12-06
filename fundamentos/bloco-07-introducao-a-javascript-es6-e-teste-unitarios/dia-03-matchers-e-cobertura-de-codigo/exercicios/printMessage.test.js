5a8cca77ec9a1f7b1da0faf588e7e68293a9771a
murilo <murilocarrijoadm@gmail.com>
2022-12-06 20:06:31 -0300

    feat: bloco 7 dia 3 - exercício 4 - Implementando teste para a exceção

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-03-matchers-e-cobertura-de-codigo/exercicios/printMessage.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-03-matchers-e-cobertura-de-codigo/exercicios/printMessage.test.js
@@ -13,4 +13,14 @@ describe('Test printMessage and info', () => {
   test('Test if the function printMessage return "Margarida,"', () => {
     expect(printMessage(info)).toContain(info.personagem);
   });
+
+  test('Test if the function return Error if the object not contain "personagem"', () => {
+    const newInfo = {
+      origem: 'Pato Donald',
+      nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    };
+
+    expect(() => { printMessage(newInfo) }).toThrow(new Error('objeto inválido'));
+    expect(() => {printMessage() }).toThrow(new Error('objeto inválido'));
+  });
 });
