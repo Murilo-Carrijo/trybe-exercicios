b3b0c548910b5953035eb8d6b0e2307297b075dc
murilo <murilocarrijoadm@gmail.com>
2022-12-06 19:34:22 -0300

    feat: bloco 7 dia 3 - exercício 1 - Implementando teste para o caso de erro sum("4", 0) = 0

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-03-matchers-e-cobertura-de-codigo/exercicios/sum.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-03-matchers-e-cobertura-de-codigo/exercicios/sum.test.js
@@ -8,4 +8,8 @@ describe('Test sum function', () => {
   test('Test if sum(0, 0) return 0', () => {
     expect(sum(0, 0)).toBe(0);
   });
+
+  test('Test if sum("4", 0) return Error', () => {
+    expect(() => { sum('4', 0) }).toThrow();
+  });
 });
