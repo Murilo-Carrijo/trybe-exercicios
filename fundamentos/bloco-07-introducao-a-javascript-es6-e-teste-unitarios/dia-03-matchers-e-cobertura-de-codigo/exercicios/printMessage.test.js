d11afb191d119c46a9deee6209a9e86ab4c8ba77
murilo <murilocarrijoadm@gmail.com>
2022-12-06 19:59:04 -0300

    feat: bloco 7 dia 3 - exercício 4 - Implementando caso de exceção a função printMessage

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-03-matchers-e-cobertura-de-codigo/exercicios/printMessage.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-03-matchers-e-cobertura-de-codigo/exercicios/printMessage.test.js
@@ -9,4 +9,8 @@ describe('Test printMessage and info', () => {
   test('Test if the function printMessage return "Boas vindas,"', () => {
     expect(printMessage(info)).toContain('Boas vindas,');
   });
+
+  test('Test if the function printMessage return "Margarida,"', () => {
+    expect(printMessage(info)).toContain(info.personagem);
+  });
 });
