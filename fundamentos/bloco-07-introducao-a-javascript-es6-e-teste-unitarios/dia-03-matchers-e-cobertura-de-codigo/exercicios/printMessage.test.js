22f209c825559bbb0f8ce10ec621afe9db4630d7
murilo <murilocarrijoadm@gmail.com>
2022-12-06 19:53:55 -0300

    feat: bloco 7 dia 3 - exercício 3 - Implementando teste para verificar se o retorno da função printMessage tem o texto "Boas vindas"

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-03-matchers-e-cobertura-de-codigo/exercicios/printMessage.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-03-matchers-e-cobertura-de-codigo/exercicios/printMessage.test.js
@@ -1,8 +1,12 @@
-const { info } =require('./printMessage');
+const { info, printMessage } =require('./printMessage');
 
 
 describe('Test printMessage and info', () => {
   test('Test if info contain the property "personagem"', () => {
     expect(info).toHaveProperty('personagem');
+  });
+
+  test('Test if the function printMessage return "Boas vindas,"', () => {
+    expect(printMessage(info)).toContain('Boas vindas,');
   });
 });
