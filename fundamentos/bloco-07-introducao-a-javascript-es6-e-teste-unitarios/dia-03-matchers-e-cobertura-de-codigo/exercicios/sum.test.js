a4bcf6f5cb2b6d8305f05cf576201c77d8a8dfb5
murilo <murilocarrijoadm@gmail.com>
2022-12-06 19:33:42 -0300

    feat: bloco 7 dia 3 - exercício 1 - Implementando teste para o caso de sucesso sum(0, 0) = 0

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-03-matchers-e-cobertura-de-codigo/exercicios/sum.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-03-matchers-e-cobertura-de-codigo/exercicios/sum.test.js
@@ -4,4 +4,8 @@ describe('Test sum function', () => {
   test('Test if sum(4, 5) return 9', () => {
     expect(sum(4, 5)).toBe(9);
   });
+
+  test('Test if sum(0, 0) return 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
 });
